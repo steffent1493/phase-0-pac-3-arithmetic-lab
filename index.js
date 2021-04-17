76cf2cee8a20bfa9a4d67c50b3583e48c1153dd0
steffent1493 <steffent1493@gmail.com>
2021-04-17 18:46:19 -0400

    Assignment complete

---
+++ index.js
@@ -0,0 +1,24 @@
+function add(a, b) {
+    return a+b
+}
+function subtract(a, b) {
+    return a-b
+}
+function multiply(a, b) {
+    return a*b
+}
+function divide(a, b) {
+    return a/b
+}
+function increment(n) {
+    return (n + 1);
+}
+function decrement(n) {
+    return (n - 1);
+}
+function makeInt(n, radix) {
+    return parseInt(n, 10)
+}
+function preserveDecimal(n, radix) {
+    return parseFloat(n, 10)
+}
